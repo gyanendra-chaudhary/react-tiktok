14982d9d7e936affeb6e07c9bb0798e268eeafaa
gyanendra <chaudharygyane699@gmail.com>
2021-07-15 17:05:18 +0545

    working on footer

--- src/Video.js
+++ src/Video.js
@@ -24,7 +24,7 @@ function Video() {
         ref={videoRef}
         src="https://v16m.tiktokcdn.com/0056099599aa16f46f27c487607e890e/60ef3b94/video/tos/alisg/tos-alisg-pve-0037/f484250dc2b847248b5e985f16ea27a9/?a=1180&br=3216&bt=1608&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=3&ds=3&er=&l=202107141331220102451442002F2ACA6B&lr=tiktok&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anBweGo7ZXhsNjMzODgzNEApN2Q7OjQ8Zzs3N2U3OmRnNGdsY3BvcnI0bDJgLS1kLy1zcy9iYjAvNjA1NTBeM2IzMTU6Yw%3D%3D&vl=&vr="
       ></video>
-      <VideoFooter />
+      <VideoFooter channel="gun7pro" description="i love this song" song="name jalebi baee" />
       {/* Video Footer */}
       {/* Video Sidebar */}
     </div>
@@ -32,3 +32,7 @@ function Video() {
 }
 
 export default Video;
+
+
+
+// 1:45 hr:min
--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,26 +1,35 @@
 import React from "react";
 import "./VideoFooter.css";
 
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
-import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+// import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+// import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+// import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import MusicNote from "@material-ui/icons/MusicNote";
 
 import Ticker from "react-ticker";
 
-function VideoFooter() {
+function VideoFooter({channel, description, song}) {
     return (
         <div className="videoFooter">
             <div className="videoFooter__text">
-                <h3>@user123</h3>
-                <p>this is a description</p>
-                <Ticker mode="smooth">
+                <h3>@{channel}</h3>
+                <p>{description}</p>
+                <div className="videoFooter__ticker">
+                <MusicNote className="videoFooter__icon" />
+                <Ticker className="ticker" mode="smooth">
                     {
-
+                        ({ index }) => (
+                            <>
+                                <p>{song }</p>
+                            </>
+                        )
                     }
                 </Ticker>
+                </div>
+                
             </div>
             <img className="videoFooter__record" src="https://as1.ftcdn.net/jpg/02/18/30/78/500_F_218307885_qoGgm65BIz7DcXpKeK1MeeubSWarIULy.jpg" alt="" />
-        </div>
+        </div >
     );
 }
 
